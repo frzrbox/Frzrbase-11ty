bbdbfa1ed29e026a7531f226db86a760f5c53575
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-05-22 21:54:46 -1000

    feat: projects configuration uses gulp to compile babel and sass

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,1 +1,1 @@
-console.log('Javascript connected');
+!function(e){"function"==typeof define&&define.amd?define("scripts",["core-js"],e):e()}((function(){"use strict";console.log("this is the imported message")}));
